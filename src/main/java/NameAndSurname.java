713168558440a58fbe67b78f32792c3982afe7fe
sm791600 <stojanovikmarko7916@gmail.com>
2024-03-04 10:35:45 +0100

    Add NameAndSurname.java

---
+++ src/main/java/NameAndSurname.java
@@ -0,0 +1,10 @@
+
+public class NameAndSurname {
+public static void main(String[] args) {
+	
+	String name="Marko";
+	String surname="Stojanovikj";
+	
+	System.out.println(name+ " "+ surname);
+}
+}
